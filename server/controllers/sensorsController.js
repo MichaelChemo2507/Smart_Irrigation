6f4948736c6454b53f19171b34cfa9a29c5b806b
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-03-09 18:40:06 +0200

    Finish coding the algoritem of finding sensor by id.(need testing)

--- server/controllers/sensorsController.js
+++ server/controllers/sensorsController.js
@@ -2,16 +2,29 @@ const sensorsService = require("../services/sensorsService");
 const getAll = async (req, res) => {
   try {
     const sensors = await sensorsService.getAll();
-    if (sensors.length > 0) {
-      res.json(sensors);
-    } else {
-      res.status(404).json({ message: "sensrs not found" });
-    }
+
+    if (sensors.length > 0) res.json(sensors);
+    else res.status(404).json({ message: "sensrs not found" });
+
   } catch (error) {
     console.error("Error in getAllSensors:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
+const findSensorById = async (req, res) => {
+  try {
+    let { id } = req.params;
+    const sensor = await sensorsService.findSensorById(id);
+
+    if (sensor) res.json(sensor);
+    else res.status(404).json({ message: "sensr not found" });
+
+  } catch (error) {
+    console.error("Error in Finding Sensor By Id:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 module.exports = {
   getAll: getAll,
+  findSensorById:findSensorById
 };
--- server/services/sensorsService.js
+++ server/services/sensorsService.js
@@ -1,8 +1,14 @@
-const Sensors = require('../models/sensors');
+const Sensors = require('../models/sensorsModel');
 const getAll = async () => {
     return await Sensors.getAll();
 }
+const findSensorById = async (value) => {
+    let rows = await Sensors.findSensorById(value);
+    console.log(rows);
+    return rows;
+}
 
 module.exports = {
-    getAll: getAll
+    getAll: getAll,
+    findSensorById:findSensorById
 }
