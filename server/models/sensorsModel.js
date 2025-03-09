ebdc6cf312210dfd08786bc6af4bde5ce39a5a3f
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-03-09 19:54:07 +0200

    Finish the add sensor feature.(tested with Insomania) Added some error handlers.

--- server/models/sensorsModel.js
+++ server/models/sensorsModel.js
@@ -15,11 +15,34 @@ class Sensors {
   static async findSensorById(values) {
     try {
       const sql = "SELECT * FROM `sensors` WHERE `id` = ?";
-      const [rows, fields] = await connection.pool.execute(sql,[values]);
+      const [rows, fields] = await connection.pool.execute(sql, [values]);
       return rows;
     } catch (err) {
       console.log(err.message);
     }
   }
+  static async addSensor(values) {
+    try {
+      if (!values) throw new Error("invalid values received!");
+      if (values.length > 2 || values.length < 1)
+        throw new RangeError("Error in the number of values received!");
+      if (typeof values[0] != typeof "")
+        throw new TypeError(
+          `invalid name type! - receive type ${typeof values[0]}`
+        );
+      if (typeof values[1] != typeof 1)
+        throw new TypeError(
+          `invalid isRunning type! - receive type ${typeof values[1]}`
+        );
+      const sql = "INSERT INTO `sensors`(`name`, `isRunning`) VALUES (?, ?)";
+      const [rows, fields] = await connection.pool.execute(sql, values);
+      return rows;
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+  static async addMultipleSensors(values) {}
+  static async updateSensor(values) {}
+  static async deleteSensor(values) {}
 }
 module.exports = Sensors;
