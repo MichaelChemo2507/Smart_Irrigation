7ce48308c39938d2da53f87b189cf33cd3d224af
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-03-09 18:40:06 +0200

    Finish coding the algoritem of finding sensor by id.(need testing)

--- server/models/sensorsModel.js
+++ server/models/sensorsModel.js
@@ -12,5 +12,14 @@ class Sensors {
       console.log(err);
     }
   }
+  static async findSensorById(values) {
+    try {
+      const sql = "SELECT * FROM `sensors` WHERE `id` = ?";
+      const [rows, fields] = await connection.pool.execute(sql,[values]);
+      return rows;
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
 }
 module.exports = Sensors;
--- server/routes/sensorsRout.js
+++ server/routes/sensorsRout.js
@@ -4,5 +4,6 @@ const sensoresController = require('../controllers/sensorsController');
 const router = express.Router();
 
 router.get('/',sensoresController.getAll);
+router.get('/:id',sensoresController.findSensorById);
 
 module.exports = router;
