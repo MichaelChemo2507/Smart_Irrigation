7fbff5b78ed33ab0f514fdc395d7171f395aaa9c
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-03-10 00:39:13 +0200

    Adding the delete option.(tested with Insomnia) 404 code for unexisted id. 204 code for seccesfull delete.

--- server/services/sensorsService.js
+++ server/services/sensorsService.js
@@ -1,19 +1,26 @@
-const Sensors = require('../models/sensorsModel');
+const Sensors = require("../models/sensorsModel");
 const getAll = async () => {
-    return await Sensors.getAll();
-}
+  return await Sensors.getAll();
+};
 const findSensorById = async (values) => {
-    let rows = await Sensors.findSensorById(values);
-    return rows;
-}
+  let rows = await Sensors.findSensorById(values);
+  if (rows == undefined) throw new Error("undefinde received!");
+  return rows;
+};
 const addSensor = async (values) => {
-    let rows = await Sensors.addSensor(values);
-    if (rows == undefined) throw new Error("undefinde received!");
-    return rows.insertId;
-}
+  let rows = await Sensors.addSensor(values);
+  if (rows == undefined) throw new Error("undefinde received!");
+  return rows.affectedRows;
+};
+const deleteSensor = async (values) => {
+  let rows = await Sensors.deleteSensor(values);
+  if (rows == undefined) throw new Error("undefinde received!");
+  return rows.affectedRows;
+};
 
 module.exports = {
-    getAll: getAll,
-    findSensorById: findSensorById,
-    addSensor:addSensor
-}
+  getAll: getAll,
+  findSensorById: findSensorById,
+  addSensor: addSensor,
+  deleteSensor: deleteSensor,
+};
