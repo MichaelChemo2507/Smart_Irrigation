e25baa6f33ff55b82f999ba78e6c0a6cd2bbdeee
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-03-10 20:55:55 +0200

    Finish the species model.(Nedd testing) Need more validation. Missing handling foreign key.

--- server/models/speciesModel.js
+++ server/models/speciesModel.js
@@ -1,29 +1,48 @@
 const connection = require("../config/db");
 
-class Sensors {
+class Species {
   static async getAll() {
     try {
-      const sql = "SELECT * FROM `sensors`";
+      const sql = "SELECT * FROM `species`";
       const [rows, fields] = await connection.pool.query({
         sql,
       });
       return rows;
     } catch (err) {
-      console.error("Error in sensorsModel level! - ", err.message);
+      console.error("Error in speciesModel level! - ", err.message);
       throw err;
     }
   }
-  static async findSensorById(values) {
+  static async findSpeciesById(values) {
     try {
-      const sql = "SELECT * FROM `sensors` WHERE `id` = ?";
+      const sql = "SELECT * FROM `species` WHERE `id` = ?";
       const [rows, fields] = await connection.pool.execute(sql, [values]);
       return rows;
     } catch (err) {
-      console.error("Error in sensorsModel level! - ", err.message);
+      console.error("Error in speciesModel level! - ", err.message);
       throw err;
     }
   }
-  static async addSensor(values) {
+  static async addSpecies(values) {
+    try {
+      if (!values) throw new Error("invalid values received!");
+      if (!Array.isArray(values)) values = [values];
+      if (values.length > 1 || values.length < 1)
+        throw new RangeError("Error in the number of values received!");
+      if (typeof values[0] != typeof "")
+        throw new TypeError(
+          `invalid name type! - receive type ${typeof values[0]}`
+        );
+      const sql = "INSERT INTO `species`(`name`) VALUES (?)";
+      const [rows, fields] = await connection.pool.execute(sql, values);
+      return rows;
+    } catch (err) {
+      console.error("Error in speciesModel level! - ", err.message);
+      throw err;
+    }
+  }
+  static async addMultipleSpecies(values) {}
+  static async updateSpecies(values) {
     try {
       if (!values) throw new Error("invalid values received!");
       if (!Array.isArray(values)) values = [values];
@@ -33,39 +52,11 @@ class Sensors {
         throw new TypeError(
           `invalid name type! - receive type ${typeof values[0]}`
         );
-      if (typeof values[1] != typeof 1)
-        throw new TypeError(
-          `invalid isRunning type! - receive type ${typeof values[1]}`
-        );
-      const sql = "INSERT INTO `sensors`(`name`, `isRunning`) VALUES (?, ?)";
+      const sql = "UPDATE `species` SET `name` = ? WHERE `id` = ?";
       const [rows, fields] = await connection.pool.execute(sql, values);
       return rows;
     } catch (err) {
-      console.error("Error in sensorsModel level! - ", err.message);
-      throw err;
-    }
-  }
-  static async addMultipleSensors(values) {}
-  static async updateSensor(values) {
-    try {
-      if (!values) throw new Error("invalid values received!");
-      if (!Array.isArray(values)) values = [values];
-      if (values.length > 3 || values.length < 1)
-        throw new RangeError("Error in the number of values received!");
-      if (typeof values[0] != typeof "")
-        throw new TypeError(
-          `invalid name type! - receive type ${typeof values[0]}`
-        );
-      if (typeof values[1] != typeof 1)
-        throw new TypeError(
-          `invalid isRunning type! - receive type ${typeof values[1]}`
-        );
-      const sql =
-        "UPDATE `sensors` SET `name` = ?, `isRunning` = ? WHERE `id` = ?";
-      const [rows, fields] = await connection.pool.execute(sql, values);
-      return rows;
-    } catch (err) {
-      console.error("Error in sensorsModel level! - ", err.message);
+      console.error("Error in speciesModel level! - ", err.message);
       throw err;
     }
   }
@@ -73,14 +64,14 @@ class Sensors {
     try {
       if (!values) throw new Error("invalid values received!");
       if (!Array.isArray(values)) values = [values];
-      const sql = "DELETE FROM `sensors` WHERE `id` = ?";
+      const sql = "DELETE FROM `species` WHERE `id` = ?";
       const [rows, fields] = await connection.pool.execute(sql, [values]);
 
       return rows;
     } catch (err) {
-      console.error("Error in sensorsModel level! - ", err.message);
+      console.error("Error in speciesModel level! - ", err.message);
       throw err;
     }
   }
 }
-module.exports = Sensors;
+module.exports = Species;
