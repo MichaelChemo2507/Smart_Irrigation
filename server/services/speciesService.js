3cf6747f1500f7bd07196dc45acf6889071ed2c2
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-03-10 21:16:01 +0200

    Finish the  species service and change it to a class.(Need testing) Also update the sensors service to a class.

---
+++ server/services/speciesService.js
@@ -0,0 +1,55 @@
+const Species = require("../models/speciesModel");
+class SpeciesService {
+  static async getAll() {
+    try {
+      let rows = await Species.getAll();
+      if (rows == undefined) throw new Error("No rows received from the DB!");
+      return rows;
+    } catch (err) {
+      console.error(`Error in service getAll: ${err.message}`);
+      throw new Error("Failed to update species due to an internal error.");
+    }
+  }
+  static async findSpeciesById(values) {
+    try {
+      let rows = await Species.findSpeciesById(values);
+      if (rows == undefined) throw new Error("No rows received from the DB!");
+      return rows;
+    } catch (err) {
+      console.error(`Error in service findSpeciesById: ${err.message}`);
+      throw new Error("Failed to update species due to an internal error.");
+    }
+  }
+  static async addSpecies(values) {
+    try {
+      let rows = await Species.addSpecies(values);
+      if (rows == undefined) throw new Error("No rows received from the DB!");
+      return rows.affectedRows;
+    } catch (err) {
+      console.error(`Error in service addSpecies: ${err.message}`);
+      throw new Error("Failed to update species due to an internal error.");
+    }
+  }
+  static async deleteSpecies(values) {
+    try {
+      let rows = await Species.deleteSpecies(values);
+      if (rows == undefined) throw new Error("No rows received from the DB!");
+      return rows.affectedRows;
+    } catch (err) {
+      console.error(`Error in service deleteSpecies: ${err.message}`);
+      throw new Error("Failed to update species due to an internal error.");
+    }
+  }
+  static async updateSpecies(values) {
+    try {
+      let rows = await Species.updateSpecies(values);
+      if (rows === undefined) throw new Error("No rows received from the DB!");
+      return rows.affectedRows;
+    } catch (err) {
+      console.error(`Error in service updateSpecies: ${err.message}`);
+      throw new Error("Failed to update species due to an internal error.");
+    }
+  }
+}
+
+module.exports = SpeciesService;
