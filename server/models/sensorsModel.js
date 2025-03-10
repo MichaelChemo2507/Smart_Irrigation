7c6ee7ddca7a57e0ce21708d9c475e5370d85df7
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-03-10 15:49:50 +0200

    Finish the updating db row at sensors table.(Tested manually with Insomania) Added some error handllers to the controller.

--- server/models/sensorsModel.js
+++ server/models/sensorsModel.js
@@ -42,11 +42,34 @@ class Sensors {
     }
   }
   static async addMultipleSensors(values) {}
-  static async updateSensor(values) {}
+  static async updateSensor(values) {
+    try {
+      console.log(values);
+      
+      if (!values) throw new Error("invalid values received!");
+      if (values.length > 3 || values.length < 1)
+        throw new RangeError("Error in the number of values received!");
+      if (typeof values[0] != typeof "")
+        throw new TypeError(
+          `invalid name type! - receive type ${typeof values[0]}`
+        );
+      if (typeof values[1] != typeof 1)
+        throw new TypeError(
+          `invalid isRunning type! - receive type ${typeof values[1]}`
+        );
+      const sql =
+        "UPDATE `sensors` SET `name` = ?, `isRunning` = ? WHERE `id` = ?";
+      const [rows, fields] = await connection.pool.execute(sql, values);
+      return rows;
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
   static async deleteSensor(values) {
     try {
       const sql = "DELETE FROM `sensors` WHERE `id` = ?";
       const [rows, fields] = await connection.pool.execute(sql, [values]);
+
       return rows;
     } catch (err) {
       console.log(err.message);
