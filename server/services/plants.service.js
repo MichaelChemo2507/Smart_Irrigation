df0b3c3b3d33e80889982593849d38661f487972
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-03-12 16:05:27 +0200

    Finish the plants service.

---
+++ server/services/plants.service.js
@@ -0,0 +1,55 @@
+const Plants = require("../models/plants.model");
+class PlantsService {
+  static async getAll() {
+    try {
+      let rows = await Plants.getAll();
+      if (rows == undefined) throw new Error("No rows received from the DB!");
+      return rows;
+    } catch (err) {
+      console.error(`Error in service getAll: ${err.message}`);
+      throw new Error("Failed to get Plants due to an internal error.");
+    }
+  }
+  static async findPlantById(values) {
+    try {
+      let rows = await Plants.findSpeciesById(values);
+      if (rows == undefined) throw new Error("No rows received from the DB!");
+      return rows;
+    } catch (err) {
+      console.error(`Error in service findPlantById: ${err.message}`);
+      throw new Error("Failed to find Plant due to an internal error.");
+    }
+  }
+  static async addPlant(values) {
+    try {
+      let rows = await Plants.addPlant(values);
+      if (rows == undefined) throw new Error("No rows received from the DB!");
+      return rows.affectedRows;
+    } catch (err) {
+      console.error(`Error in service addPlant: ${err.message}`);
+      throw new Error("Failed to add Plant due to an internal error.");
+    }
+  }
+  static async deletePlants(values) {
+    try {
+      let rows = await Plants.deletePlant(values);
+      if (rows == undefined) throw new Error("No rows received from the DB!");
+      return rows.affectedRows;
+    } catch (err) {
+      console.error(`Error in service deletePlants: ${err.message}`);
+      throw new Error("Failed to delete Plant due to an internal error.");
+    }
+  }
+  static async updatePlants(values) {
+    try {
+      let rows = await Plants.updatePlant(values);
+      if (rows === undefined) throw new Error("No rows received from the DB!");
+      return rows.affectedRows;
+    } catch (err) {
+      console.error(`Error in service updatePlant: ${err.message}`);
+      throw new Error("Failed to update Plant due to an internal error.");
+    }
+  }
+}
+
+module.exports = PlantsService;
