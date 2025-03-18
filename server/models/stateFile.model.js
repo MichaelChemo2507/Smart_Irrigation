d413cee2a45e172bb7b0d118530bb881b2083b75
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-03-19 01:35:55 +0200

    Finish working on the stateData method.(tested) Ned verification

--- server/models/stateFile.model.js
+++ server/models/stateFile.model.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 const state = require("../stateFile.json");
- 
+
 // fs.writeFile(
 //     "stateFile.json",
 //     JSON.stringify(),
 //     err => {
 //         if (err) throw err;
 //         console.log("Done writing");
-//     }); 
+//     });
 class StateFile {
   static async getAll() {
     try {
@@ -16,15 +16,22 @@ class StateFile {
       console.error("Error in StateFile Model level! - ", err.message);
       throw err;
     }
+  }
+  static async getStatusDataByStatusName(values) {
+    try {
+      return state[values];
+    } catch (err) {
+      console.error("Error in StateFile Model level! - ", err.message);
+      throw err;
     }
-    static async getStatusDataByStatusName(values) {
-
-        try {
-            return state[values];
-          } catch (err) {
-            console.error("Error in StateFile Model level! - ", err.message);
-            throw err;
-          } 
+  }
+  static async getStateData() {
+    try {
+      return { state: state.state, time: state.time };
+    } catch (err) {
+      console.error("Error in StateFile Model level! - ", err.message);
+      throw err;
     }
+  }
 }
 module.exports = StateFile;
