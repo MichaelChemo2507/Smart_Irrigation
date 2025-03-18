f3f38d32a8a8aad2a4282619bfdf672dd22543c1
MichaelChemo2507 <131193640+MichaelChemo2507@users.noreply.github.com>
2025-03-19 01:20:56 +0200

    Finish workig on the getStatusDataByStatusName method.(tested). need to add some validation.

--- server/routes/stateFile.rout.js
+++ server/routes/stateFile.rout.js
@@ -4,5 +4,6 @@ const StateFileController = require('../controllers/stateFile.controller');
 const router = express.Router();
 
 router.get('/',StateFileController.getAll);
+router.get('/:statusMode',StateFileController.getStatusDataByStatusName);
 
 module.exports = router;
